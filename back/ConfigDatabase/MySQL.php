93fdaece5962beab5ce05345401056cec0279728
loic <loic.hernandez@sfr.fr>
2022-10-01 17:41:57 +0200

    credentials mysql

--- back/ConfigDatabase/MySQL.php
+++ back/ConfigDatabase/MySQL.php
@@ -3,7 +3,12 @@
 function getBdd()
 {
   try {
-    $pdo = new PDO("mysql:host=localhost;dbname=jason;charset=utf8", "root", "root", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    if ($_SERVER['HTTP_HOST'] === "localhost") {
+    $pdo = new PDO(PDO_LOCALHOST_CONNEXION, USER_LOCALHOST, PASSWORD_LOCALHOST, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    } else {
+    $pdo = new PDO(PDO_SERVER_CONNEXION, USER_SERVER, PASSWORD_SERVER, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+    }
     return $pdo;
   } catch (Exception $e) {
 
